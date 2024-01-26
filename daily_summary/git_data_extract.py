a464d38a453222620017ddd73040b5b968e8cee4
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2024-01-26 00:37:21 -0500

    feat: Updated daily_summary/git_data_extract.py

--- daily_summary/git_data_extract.py
+++ daily_summary/git_data_extract.py
@@ -4,7 +4,7 @@ import json
 
 
 # Replace with your repository's path
-def extract_git_data(repo_path, author):
+def extract_git_data(repo_path, author, date):
     repo = git.Repo(repo_path)
 
     today = datetime.now().date()
