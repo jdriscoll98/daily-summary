b9288761643cd39039a7ff90521c784ffb69b96d
Jack Driscoll <jdriscoll98@ufl.edu>
2024-01-25 21:14:31 -0500

    fix first commit bug

--- git-data-extract.py
+++ git-data-extract.py
@@ -1,13 +1,21 @@
 import git
 
 # Replace with your repository's path
-repo_path = '.'
+repo_path = '../mxlocker-web'
 repo = git.Repo(repo_path)
+
+first_commit = list(repo.iter_commits())[-1].hexsha
 
 for commit in repo.iter_commits():
     print('Commit:', commit.hexsha)
     print('Author:', commit.author.name)
     print('Date:', commit.authored_datetime)
-    print('Message:', commit.message)
-    print('Diff:', repo.git.diff(commit.hexsha, commit.hexsha + '^'))
+    print('Message:', commit.message.strip())
+
+    # Check if it's the first commit
+    if commit.hexsha != first_commit:
+        print('Diff:', repo.git.diff(commit.hexsha + '^', commit.hexsha))
+    else:
+        print('Diff: First commit in the repository')
+
     print('-' * 40)
